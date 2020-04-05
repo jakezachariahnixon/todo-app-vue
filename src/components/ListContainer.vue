1e06ab9093b1babf9da3643fe499e6868e27b17e
jakezachariahnixon <jake.zachariah.nixon@gmail.com>
2020-04-05 10:54:22 +0100

    style: run prettify.js on components/ListContainer.vue

--- src/components/ListContainer.vue
+++ src/components/ListContainer.vue
@@ -6,24 +6,30 @@
       +
     </button>
     <div id="addTaskInterface">
-        <div id="closeAddTaskInterface" v-on:click="this.closeAddTask">&times;</div>
-        <textarea id="taskName" v-model="newTask" placeholder="What do you need to do?">
-        </textarea>
-        <div id="priorityContainer">
-            <h2>Priority</h2>
-            <button class="priorityBtn" id="lowPriority">Low</button>
-            <button class="priorityBtn" id="midPriority">Medium</button>
-            <button class="priorityBtn" id="highPriority">High</button>
-        </div>
-        <div id="parentContainer">
-            <h2>Parent</h2>
-            <select>
-                <option value="none">None</option>
-                <option value="1">Task One</option>
-                <option value="2">Task Two</option>
-                <option value="3">Task Three</option>
-            </select>
-        </div>
+      <div id="closeAddTaskInterface" v-on:click="this.closeAddTask">
+        &times;
+      </div>
+      <textarea
+        id="taskName"
+        v-model="newTask"
+        placeholder="What do you need to do?"
+      >
+      </textarea>
+      <div id="priorityContainer">
+        <h2>Priority</h2>
+        <button class="priorityBtn" id="lowPriority">Low</button>
+        <button class="priorityBtn" id="midPriority">Medium</button>
+        <button class="priorityBtn" id="highPriority">High</button>
+      </div>
+      <div id="parentContainer">
+        <h2>Parent</h2>
+        <select>
+          <option value="none">None</option>
+          <option value="1">Task One</option>
+          <option value="2">Task Two</option>
+          <option value="3">Task Three</option>
+        </select>
+      </div>
     </div>
   </div>
 </template>
@@ -38,7 +44,7 @@ export default {
   data() {
     return {
       todos: [],
-      newTask: '',
+      newTask: "",
       addOpen: false
     };
   },
@@ -58,41 +64,39 @@ export default {
     TaskList
   },
   methods: {
-      addTodo() {
-          if (this.newTask.length < 1){
-              document.getElementById("taskName").style.borderColor = "#e25555";
-          } else {
-            this.todos.push({ title: this.newTask, completed: false});
-            this.newTask = '';
-            document.getElementById("taskName").style.borderColor = "#e5e5e5";
-          }
-      },
-      displayNoNameError() {
-          document.getElementById("taskName").style.borderColor = "#e25555";
-      },
-      handleAddButton() {
-          if (! this.addOpen){
-            this.openAddTask();
-          } else {
-              alert("yeet");
-            this.addTodo();
-          }
-          },
-      openAddTask() {
-          document.getElementById("addTaskInterface").style.display = "block";
-          document.getElementById("addTaskInterface").style.opacity = "1";
-          this.addOpen = true;
-      },
-      closeAddTask() {
-          this.addOpen = false;
-          document.getElementById("addTaskInterface").style.opacity = "0";
-          setTimeout(
-              function () {
-                  document.getElementById("addTaskInterface").style.display = "none";
-              }
-            , 500);
+    addTodo() {
+      if (this.newTask.length < 1) {
+        document.getElementById("taskName").style.borderColor = "#e25555";
+      } else {
+        this.todos.push({ title: this.newTask, completed: false });
+        this.newTask = "";
+        document.getElementById("taskName").style.borderColor = "#e5e5e5";
       }
-  },
+    },
+    displayNoNameError() {
+      document.getElementById("taskName").style.borderColor = "#e25555";
+    },
+    handleAddButton() {
+      if (!this.addOpen) {
+        this.openAddTask();
+      } else {
+        alert("yeet");
+        this.addTodo();
+      }
+    },
+    openAddTask() {
+      document.getElementById("addTaskInterface").style.display = "block";
+      document.getElementById("addTaskInterface").style.opacity = "1";
+      this.addOpen = true;
+    },
+    closeAddTask() {
+      this.addOpen = false;
+      document.getElementById("addTaskInterface").style.opacity = "0";
+      setTimeout(function() {
+        document.getElementById("addTaskInterface").style.display = "none";
+      }, 500);
+    }
+  }
 };
 </script>
 
@@ -165,109 +169,109 @@ export default {
 }
 // Add Task styling
 #closeAddTaskInterface {
-    width: 100%;
-    height: 0%;
-    font-size: 24px;
-    margin-left: 93%;
-    transform: translateY(8px);
-    color: #808080;
-    transition: 0.1s;
-    cursor: pointer;
+  width: 100%;
+  height: 0%;
+  font-size: 24px;
+  margin-left: 93%;
+  transform: translateY(8px);
+  color: #808080;
+  transition: 0.1s;
+  cursor: pointer;
 }
 #closeAddTaskInterface:hover {
-    color: #404040;
-    transition: 0.1s;
+  color: #404040;
+  transition: 0.1s;
 }
 #addTaskInterface {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 50%;
-    background-color: $lightHighlight;
-    border-radius: 0 0 10px 10px;
-    z-index: 2;
-    display: none;
-    opacity: 0;
-    transition: opacity 0.5s;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50%;
+  background-color: $lightHighlight;
+  border-radius: 0 0 10px 10px;
+  z-index: 2;
+  display: none;
+  opacity: 0;
+  transition: opacity 0.5s;
 }
 #taskName {
-    width: 80%;
-    background-color: $white;
-    border: 3px solid $lightHighlight;
-    border-radius: 5px;
-    outline: none;
-    box-shadow: none !important;
-    margin-top: 10%;
-    margin-left: 5%;
-    padding: 5%;
-    height: 30%;
-    font-family: Proxima Nova;
-    font-size: 1vw;
-    resize: none;
+  width: 80%;
+  background-color: $white;
+  border: 3px solid $lightHighlight;
+  border-radius: 5px;
+  outline: none;
+  box-shadow: none !important;
+  margin-top: 10%;
+  margin-left: 5%;
+  padding: 5%;
+  height: 30%;
+  font-family: Proxima Nova;
+  font-size: 1vw;
+  resize: none;
 }
 #taskName:focus {
-    border-color: $lightHighlight;
-    transition: 0.25s;
+  border-color: $lightHighlight;
+  transition: 0.25s;
 }
 #priorityContainer {
-    width: 100%;
-    height: 10%;
-    padding-top: 2%;
-    font-family: Proxima Nova;
-    font-size: 0.7vw;
+  width: 100%;
+  height: 10%;
+  padding-top: 2%;
+  font-family: Proxima Nova;
+  font-size: 0.7vw;
 }
 #priorityContainer h2 {
-    margin: 0 0 0 0;
-    padding: 0 0 0 0;
-    display: inline-block;
-    margin-left: 10%;
-    width: 30%;
+  margin: 0 0 0 0;
+  padding: 0 0 0 0;
+  display: inline-block;
+  margin-left: 10%;
+  width: 30%;
 }
 .priorityBtn {
-    width: 70px;
-    height: 20px;
-    border-radius: 15px;
-    border: 1px solid;
-    margin-right: 5px;
-    cursor: pointer;
+  width: 70px;
+  height: 20px;
+  border-radius: 15px;
+  border: 1px solid;
+  margin-right: 5px;
+  cursor: pointer;
 }
 #lowPriority {
-    color: $lowPriority;
-    border-color: $lowPriority;
+  color: $lowPriority;
+  border-color: $lowPriority;
 }
 #lowPriority:focus {
-    color: $lightHighlight;
-    background-color: $lowPriority;
+  color: $lightHighlight;
+  background-color: $lowPriority;
 }
 #midPriority {
-    color: $midPriority;
+  color: $midPriority;
 }
 #midPriority:focus {
-    color: $lightHighlight;
-    background-color: $midPriority;
+  color: $lightHighlight;
+  background-color: $midPriority;
 }
 #highPriority {
-    color: $highPriority;
+  color: $highPriority;
 }
 #highPriority:focus {
-    color: $lightHighlight;
-    background-color: $highPriority;
+  color: $lightHighlight;
+  background-color: $highPriority;
 }
 #parentContainer {
-    width: 100%;
-    height: 10%;
-    font-family: Proxima Nova;
-    font-size: 0.7vw;
+  width: 100%;
+  height: 10%;
+  font-family: Proxima Nova;
+  font-size: 0.7vw;
 }
 #parentContainer h2 {
-    margin: 0 0 0 0;
-    padding: 0 0 0 0;
-    display: inline-block;
-    margin-left: 10%;
-    width: 30%;
+  margin: 0 0 0 0;
+  padding: 0 0 0 0;
+  display: inline-block;
+  margin-left: 10%;
+  width: 30%;
 }
 #parentContainer select {
-    width: 220px;
-    cursor: pointer;
+  width: 220px;
+  cursor: pointer;
 }
 </style>
